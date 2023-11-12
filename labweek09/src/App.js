eb9c594c4097ee7aab3b41f9eb71d515395dc182
Ajitesh82 <ajitesh82@gmail.com>
2023-11-12 14:32:11 -0500

    second commit

---
+++ labweek09/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - 1</h1>
+        <h3>React JS Programming Week09 Lab exercise</h3>
+        <h4>101395536</h4>
+        <h4>Ajitesh Kumar</h4>
+        <h4>George Brown College, Toronto</h4>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
