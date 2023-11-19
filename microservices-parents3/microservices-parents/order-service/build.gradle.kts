5d39c335899553d2f9a506f40c7a75afb7b4c0f9
Ajitesh82 <ajitesh82@gmail.com>
2023-11-18 19:06:57 -0500

    Third commit

---
+++ microservices-parents3/microservices-parents/order-service/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "ca.gbc"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-webflux:3.1.5")
+	implementation("org.springframework:spring-web:6.0.13")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
+	testImplementation("org.testcontainers:testcontainers:1.16.0")
+	testImplementation("org.testcontainers:postgresql:1.16.0")
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+	testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
+	testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+	testImplementation("org.testcontainers:postgresql:1.18.3")
+	implementation(platform("org.testcontainers-bom:1.18.3"))
+
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
